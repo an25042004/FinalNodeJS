7fea71dece66b20b5872fcd8ba10f4a622fb75f2
city314 <126061643+city314@users.noreply.github.com>
2025-09-02 14:05:19 +0700

    Update App.js for Routing
    Update App.js for Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+// User pages
+import ForgotPassword from "./pages/account/ForgotPasswordPage.jsx";
+import ResetPassword from "./pages/account/ResetPasswordPage.jsx";
+import Cart from "./pages/account/CartPage.jsx";
+import Profile from "./pages/account/AccountProfilePage.jsx";
+import ChangePassword from "./pages/account/ChangePasswordPage.jsx";
+import AddressBook from "./pages/account/AddressBookPage.jsx";
+import OrderHistory from "./pages/account/OrderHistoryPage.jsx";
+// Admin pages
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="main-content">
+      <Routes>
+        {/* User routes */}
+        <Route path="/account/:id" element={<Profile />} />
+        <Route path="/account/:id/cart" element={<Cart />} />
+        <Route path="/account/:id/change-password" element={<ChangePassword />} />
+        <Route path="/account/:id/address-book" element={<AddressBook />} />
+        <Route path="/account/:id/order-history" element={<OrderHistory />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
+        {/* Admin routes */}
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
