7d63fc6b5715d0953a6579368ee1e0a0e712fc69
city314 <126061643+city314@users.noreply.github.com>
2025-09-02 14:23:36 +0700

    Updating routing
    Updating routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
-import "./App.css";
+// import "./App.css";
 import { Route, Routes } from "react-router-dom";
 // User pages
-import ForgotPassword from "./pages/account/ForgotPasswordPage.jsx";
-import ResetPassword from "./pages/account/ResetPasswordPage.jsx";
+import ForgotPassword from "./pages/auth/ForgotPasswordPage.jsx";
+import ResetPassword from "./pages/auth/ResetPasswordPage.jsx";
 import Cart from "./pages/account/CartPage.jsx";
 import Profile from "./pages/account/AccountProfilePage.jsx";
 import ChangePassword from "./pages/account/ChangePasswordPage.jsx";
@@ -15,13 +15,18 @@ function App() {
     <div className="main-content">
       <Routes>
         {/* User routes */}
-        <Route path="/account/:id" element={<Profile />} />
-        <Route path="/account/:id/cart" element={<Cart />} />
-        <Route path="/account/:id/change-password" element={<ChangePassword />} />
-        <Route path="/account/:id/address-book" element={<AddressBook />} />
-        <Route path="/account/:id/order-history" element={<OrderHistory />} />
+        {/* mấy cái id đáng lẽ là ghi :id mà đang code frontend trước nên để id nha*/}
+        <Route path="/account/id" element={<Profile />} />
+        <Route path="/account/id/cart" element={<Cart />} />
+        <Route
+          path="/account/id/change-password"
+          element={<ChangePassword />}
+        />
+        <Route path="/account/id/address-book" element={<AddressBook />} />
+        <Route path="/account/id/order-history" element={<OrderHistory />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
+        {/* mấy cái token đáng lẽ là ghi :token mà đang code frontend trước nên để token nha*/}
+        <Route path="/reset-password/token" element={<ResetPassword />} />
         {/* Admin routes */}
       </Routes>
     </div>
